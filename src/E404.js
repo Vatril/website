9919c2cd1f7c8cddda3271782a84728abc7af710
Vatril <nikolaisummers@gmail.com>
2019-09-01 17:04:46 +0200

    added error page

--- src/E404.js
+++ src/E404.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
+import styles from "./E404.module.css"
 
 export default class E404 extends Component {
     render() {
-        return (
-            <div>
-
-            </div>
-        )
+        return <h2 id={styles.e404}>Sorry, Nothing here</h2>
     }
 }
