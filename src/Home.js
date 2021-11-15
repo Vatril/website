57c5b58541c0787f217893febf8532731fcdbfad
Nikolai Jay Summers <nikolai.summers@pandata.de>
2021-11-15 23:26:37 +0100

    updated text in home screen

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class Home extends Component {
     render() {
         return (
             <>
-                <p> Hello! </p>
-                <p> My Name is Nikolai, I am 24 and a Software Developer.
-                  This website mainly exists to provide another point of contact and giving my testing server another
-            purpose. </p>
-                <p> I am currently studying Telematics! </p>
-                <p>  My main languages are JS, Python, Ruby and Java/Kotlin.</p>
                 <p>
-                    I mostly create Webapps, but also love Android and experimenting with other Technologies. </p>
-                <p>  If you have an idea for a project, let me know! </p>
+                    Hello!
+                </p>
+                <p>
+                    My Name is Nikolai, I am
+                    {" " +
+                    Math.abs(
+                        new Date(
+                            Date.now() - new Date(1997, 9, 6)
+                        ).getUTCFullYear() - 1970
+                    ) + " "
+                    }
+                    and a dev. This website mainly exists because I need a server for testing and why not host a website
+                    on it.
+                </p>
+                <p>
+                    I am currently working as a Data Engineer!
+                </p>
+                <p>
+                    My main languages are Python and JavaScript.
+                </p>
+                <p>
+                    I mostly work with data pipelines and create various webapps.
+                    Never tired of trying out new things tho!
+                </p>
+                <p>
+                    When I am not programming, you will find me playing tabletop games or
+                    preparing the next session.
+                </p>
             </>
         )
     }
